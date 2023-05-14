48a2d9d7e2dd4d74bd6eece880467175105646bc
AlexMiloCS <alexmilcs@gmail.com>
2023-05-14 22:03:04 +0300

    modified:   __pycache__/file_manager.cpython-311.pyc 	modified:   __pycache__/round_robin.cpython-311.pyc 	modified:   round_robin.py

--- round_robin.py
+++ round_robin.py
@@ -1,4 +1,5 @@
 import heapq
+from decimal import Decimal
 class round_robin:
     def __init__(self,R,path1,path2):
         self.R=R
@@ -6,6 +7,31 @@ class round_robin:
         self.path2 = path2
         self.lower_bound = {}
         self.full_score = {}
+        self.Wk = []
+
+
+    def check_dicts(self,index1,index2,val1,val2,obj_counter):
+        if index1 in self.lower_bound:
+            self.full_score[index1]=self.lower_bound[index1]+val1
+        else:                    
+            self.lower_bound[index1]=val1+self.R[index1]
+            obj_counter +=1
+        if index2 in self.lower_bound:
+            self.full_score[index2]=self.lower_bound[index2]+val2
+        else:                  
+            self.lower_bound[index2]=val2+self.R[index2]
+            obj_counter +=1
+        return obj_counter  
+
+    def create_min_heap(self):
+        for key, value in self.lower_bound.items():
+            if key in self.full_score:
+                true_value = self.full_score[key]
+                self.Wk.append([key,true_value])
+                continue
+            self.Wk.append([key,value])
+        
+
 
     def do_the_sequence(self,k):
         with open(self.path1, 'r') as file1, open(self.path2, 'r') as file2:
@@ -14,20 +40,16 @@ class round_robin:
                 line1 = file1.readline()
                 line2 = file2.readline()
                 split_line1 = line1.split()
-                split_line2 = line2.split()               
-                if split_line1[0] in self.lower_bound:
-                    self.full_score[split_line1[0]]=self.lower_bound[split_line1[0]]+float(split_line1[1])
-                else:
-                    print((split_line1[1]),self.R[int(split_line1[0])])
-                    self.lower_bound[split_line1[0]]=float(split_line1[1])+self.R[int(split_line1[0])]
-                    obj_counter +=1
-                if split_line2[0] in self.lower_bound:
-                    self.full_score[split_line2[0]]=self.lower_bound[split_line2[0]]+float(split_line2[1])
-                else:
-                    print((split_line2[1]),self.R[int(split_line2[0])])
-                    self.lower_bound[split_line2[0]]=float(split_line1[1])+self.R[int(split_line2[0])]
-                    obj_counter +=1
-            
+                split_line2 = line2.split()    
+                val1 = float(split_line1[1]) 
+                index1 = int(split_line1[0])
+                val2 = float(split_line1[1])                 
+                index2 = int(split_line2[0])
+                obj_counter = self.check_dicts(index1,index2,val1,val2,obj_counter)
+            self.create_min_heap() 
+            print(self.Wk)
+            heapq.heapify(self.Wk)
+            print(self.Wk)           
         print(self.lower_bound)
         print(self.full_score)
             
