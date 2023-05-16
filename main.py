3a68c766c4962b3a776b059fa75d554ac45c835f
AlexMiloCS <alexmilcs@gmail.com>
2023-05-16 17:59:31 +0300

    modified:   main.py

--- main.py
+++ main.py
@@ -4,30 +4,29 @@ from brute_force import brute_force
 from round_robin import round_robin
 
 
-"""
+
 arg0 = sys.argv[1]
 arg1 = sys.argv[2]
 arg2 = sys.argv[3]
 arg3 = sys.argv[4]
-"""
-"C:\\Users\\Alekos\\Desktop\\projects\\Diaxeirisi\\assignment3\\rnd.txt"
-manager1 = file_manager("C:\\Users\\Alekos\\Desktop\\projects\\Diaxeirisi\\assignment3\\rnd.txt")
 
+#paths = "C:\\Users\\Alekos\\Desktop\\projects\\Diaxeirisi\\assignment3\\rnd.txt" "C:\\Users\\Alekos\\Desktop\\projects\\Diaxeirisi\\assignment3\\seq1.txt" "C:\\Users\\Alekos\\Desktop\\projects\\Diaxeirisi\\assignment3\\seq2.txt"
+manager1 = file_manager(arg0)
 R = manager1.create_structure()
 
 
-round1 = round_robin(R,"C:\\Users\\Alekos\\Desktop\\projects\\Diaxeirisi\\assignment3\\seq1.txt","C:\\Users\\Alekos\\Desktop\\projects\\Diaxeirisi\\assignment3\\seq2.txt",5)
+round1 = round_robin(R,arg1,arg2,int(arg3))
 Wk = round1.do_the_sequence()
 Wk.sort(reverse=True)
 print("Number of sequential accesses= ",round1.get_sequential_accesses())
 print("Top k objects:")
 for i in range(len(Wk)):
-    print(Wk[i])
+    print("{}: {}".format(Wk[i][1],Wk[i][0]))
 
 
-brute_force1 = brute_force(R,"C:\\Users\\Alekos\\Desktop\\projects\\Diaxeirisi\\assignment3\\seq1.txt","C:\\Users\\Alekos\\Desktop\\projects\\Diaxeirisi\\assignment3\\seq2.txt")
+brute_force1 = brute_force(R,arg1,arg2)
 R_brute = brute_force1.calc_all()
 R_brute.sort(reverse=True)
-print("Brute forcec Top k objects:")
-for i in range(10):
-    print(R_brute[i])
+print("Brute force Top k objects:")
+for i in range(int(arg3)):
+    print("{}: {}".format(R_brute[i][1],R_brute[i][0],))
